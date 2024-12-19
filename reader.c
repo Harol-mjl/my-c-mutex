5e6e05f78108ebc2e15ced5c7c29c7cb53843c43
Harol-mjl <harolmijaeljorgelopez@outlook.es>
2024-12-19 18:46:25 +0100

    perf(reader.c): improve and add comments

--- reader.c
+++ reader.c
@@ -26,7 +26,7 @@ int main(int argc, char *args[]){
     struct my_data *data;
     
     //Verify if the first parameter is null
-    if (args[1] == NULL) {
+    if (!args[1]) {
         fprintf(stderr, ROJO "Error, the first parameter can't be '%s'\n" RESET, args[1]);
         return ERROR;
     }
@@ -34,7 +34,6 @@ int main(int argc, char *args[]){
     //Verify if the file exist
     stack = my_stack_read(args[1]);
     if (!stack) {
-        fprintf(stderr, ROJO "Error in my_stack_read(), file '%s' not exist \n" RESET, args[1]);
         return ERROR;
     }
     
